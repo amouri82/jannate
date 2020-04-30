351bf2ae38a919b838399dcd1e531ef9d881b904
azizamouri <amouri82@gmail.com>
2020-04-30 01:44:04 +0000

    Create Vendor Form

---
+++ src/Form/VendorType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vendor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VendorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('contact')
+            ->add('address')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Vendor::class,
+        ]);
+    }
+}
